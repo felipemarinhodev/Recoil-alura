1d99cc7c6722270edcc51638faacb8975ced110b
felipemarinhodev <felipmarinho@gmail.com>
2022-03-06 22:10:49 -0800

    corrigindo bug drag and drop

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -1,38 +1,67 @@
-
-import React from 'react'
-import style from './Calendario.module.scss';
-import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
+import React from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend';
 import 'kalend/dist/styles/index.css';
-import { useRecoilValue } from 'recoil';
+
 import { listaDeEventosState } from '../../state/atom';
 
+import style from './Calendario.module.scss';
+import ptBR from './localizacao/ptBR.json';
+
 interface IKalendEvento {
-  id?: number
-  startAt: string
-  endAt: string
-  summary: string
-  color: string
+  id?: number;
+  startAt: string;
+  endAt: string;
+  summary: string;
+  color: string;
 }
 
 const Calendario: React.FC = () => {
-
   const eventosKalend = new Map<string, IKalendEvento[]>();
   const eventos = useRecoilValue(listaDeEventosState);
 
-  eventos.forEach(evento => {
-    const chave = evento.inicio.toISOString().slice(0, 10)
+  const setListaDeEventos = useSetRecoilState(listaDeEventosState);
+
+  eventos.forEach((evento) => {
+    const chave = evento.inicio.toISOString().slice(0, 10);
     if (!eventosKalend.has(chave)) {
-      eventosKalend.set(chave, [])
+      eventosKalend.set(chave, []);
     }
     eventosKalend.get(chave)?.push({
       id: evento.id,
       startAt: evento.inicio.toISOString(),
       endAt: evento.fim.toISOString(),
       summary: evento.descricao,
-      color: 'blue'
-    })
-  })
+      color: 'blue',
+    });
+  });
+
+  const onEventDragFinish: OnEventDragFinish = (
+    kalendEventoInalterado: CalendarEvent,
+    kalendEventoAtualizado: CalendarEvent
+  ) => {
+    const evento = eventos.find(
+      (evt) => evt.descricao === kalendEventoAtualizado.summary
+    );
+
+    if (evento) {
+      const eventoAtualizado = {
+        ...evento,
+      };
+      eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt);
+      eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt);
+
+      setListaDeEventos((listaAntiga) => {
+        const indice = listaAntiga.findIndex((evt) => evt.id === evento.id);
+        return [
+          ...listaAntiga.slice(0, indice),
+          eventoAtualizado,
+          ...listaAntiga.slice(indice + 1),
+        ];
+      });
+    }
+  };
+
   return (
     <div className={style.Container}>
       <Kalend
@@ -45,9 +74,10 @@ const Calendario: React.FC = () => {
         calendarIDsHidden={['work']}
         language={'customLanguage'}
         customLanguage={ptBR}
+        onEventDragFinish={onEventDragFinish}
       />
     </div>
   );
-}
+};
 
-export default Calendario
+export default Calendario;
