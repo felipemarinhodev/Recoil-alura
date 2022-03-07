f32de10d39a2a3c2ba9947ac24e69eeff8afd2d0
felipemarinhodev <felipmarinho@gmail.com>
2022-03-06 21:40:11 -0800

    excluindo um evento

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
+import { RecoilRoot } from 'recoil';
+
 import style from './App.module.scss';
+
 import Card from './components/Card';
-import Formulario from './components/Formulario';
-import { IEvento } from './interfaces/IEvento';
 import Calendario from './components/Calendario';
+import Formulario from './components/Formulario';
 import ListaDeEventos from './components/ListaDeEventos';
-import { RecoilRoot } from 'recoil'
 
 function App() {
 
@@ -29,20 +30,12 @@ function App() {
 
   const [filtro, setFiltro] = useState<Date | null>()
 
-  const adicionarEvento = (evento: IEvento) => {
-    evento.id = Math.round((new Date()).getTime() / 1000)
-    // eventos.push(evento)
-    // setEventos([...eventos])
-  }
   const alterarStatusEvento = (id: number) => {
     // const evento = eventos.find(evento => evento.id === id)
     // if (evento) {
     //   evento.completo = !evento.completo
     // }
     // setEventos([...eventos])
-  }
-  const deletarEvento = (id: number) => {
-    // setEventos([...eventos.filter(evento => evento.id !== id)])
   }
 
   const aplicarFiltro = (data: Date | null) => {
@@ -67,7 +60,6 @@ function App() {
             <ListaDeEventos
               aoFiltroAplicado={aplicarFiltro}
               aoAlterarStatus={alterarStatusEvento}
-              aoDeletarEvento={deletarEvento}
               />
           </Card>
         </div>
